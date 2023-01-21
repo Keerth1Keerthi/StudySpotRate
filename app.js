73792a2a6fe347bb1a449c16b67e5e3da446d4cb
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-01-21 18:21:39 -0500

    remove gitignore files

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 
+//Connecting to mongoose
+mongoose.set('strictQuery', true)
+mongoose.connect('mongodb://localhost:27017/study-rate', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//Setting EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-    res.send('Reached homeee')
+    res.render('home')
 })
 
 app.get('/places', (req, res) => {
-    res.send('All of the Campground places')
+    res.render('places/index')
 })
 
+
 app.get('/places/new', (req, res) => {
-    res.send('Add new place')
+    res.render('places/new')
+})
+app.post('places', (req, res) => {
+    res.send('making post')
 })
 
 app.get('/places/:id', (req, res) => {
-    res.send('Showing place ' + req.params.id)
+    res.render('places/show')
 })
 
 app.get('/places/:id/edit', (req, res) => {
-    res.send('Looking at campground ' + req.params.id)
+    res.render('places/edit')
+})
+app.put('/places/:id', (req, res) => {
+    res.send('editing place')
+})
+app.delete('/places/:id', (req, res) => {
+    res.send('deleting place')
 })
 
 app.listen(3000, () => {
