6f277127e447528dcc4a9d3f2d972865155df412
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-01-23 22:11:23 -0500

    Added Async and Custom Error Handling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+//Error Handling
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const Place = require('./models/place');
 
 //Connecting to mongoose
@@ -41,30 +44,40 @@ app.get('/places', async (req, res) => {
 app.get('/places/new', (req, res) => {
     res.render('places/new')
 })
-app.post('/places', async (req, res) => {
+app.post('/places', catchAsync(async (req, res) => {
     const place = new Place(req.body.place)
     console.log(await place.save());
-    res.send(place)
-    //res.redirect('places')
-})
+    res.redirect('places')
+}))
 
-app.get('/places/:id', async (req, res) => {
+app.get('/places/:id', catchAsync(async (req, res) => {
     const place = await Place.findById(req.params.id);
     res.render('places/show', { place })
-})
+}))
 
-app.get('/places/:id/edit', async (req, res) => {
+app.get('/places/:id/edit', catchAsync(async (req, res) => {
     const place = await Place.findById(req.params.id);
     res.render('places/edit', { place })
-})
-app.put('/places/:id', async (req, res) => {
+}))
+
+app.put('/places/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const place = await Place.findByIdAndUpdate(id, { ...req.body.place }, { new: true });
     res.redirect(`/places/${id}`);
-})
-app.delete('/places/:id', async (req, res) => {
+}))
+app.delete('/places/:id', catchAsync(async (req, res) => {
     const place = await Place.findByIdAndDelete(req.params.id);
     res.redirect('/places')
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+
+})
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 })
 
 app.listen(3000, () => {
